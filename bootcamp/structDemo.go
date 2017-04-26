5cf96b7feedf01139ac4a72cc64dac654007b05c
sagar <sagar@allygs.com>
2017-04-26 19:28:05 +0530

    struct demo

---
+++ bootcamp/structDemo.go
@@ -0,0 +1,32 @@
+package main
+
+import "time"
+import "fmt"
+
+type Bootcamp struct {
+	Lat, Lon float64
+	Date     time.Time
+}
+
+type Bootcamp1 struct {
+	Lat, Lon float64
+}
+
+func main() {
+	// intialization
+	event := Bootcamp{
+		Lat: 34.012836,
+		Lon: -118.495338,
+	}
+
+	event.Date = time.Now()
+	fmt.Printf("Event on %s, location (%f,%f)\n", event.Date, event.Lat, event.Lon)
+
+	/*Go supports the new expression to allocate a zeroed value of the requested type
+	and to return a pointer to it
+	*/
+	fmt.Println("with new ")
+	x := new(Bootcamp1)
+	y := &Bootcamp1{}
+	fmt.Println(*x == *y)
+}
